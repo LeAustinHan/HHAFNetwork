28fbc4a769949fb51abb17d1933d72d0829df98b
hanjize <hanjize@rd.tuan800.com>
2014-08-11 10:36:12 +0800

    创建HHAFNetwork工程
     创建HHAFNetwork工程

---
+++ HHAFNetwork/HHAFNetwork/HHAFNetwork.h
@@ -0,0 +1,13 @@
+//
+//  HHAFNetwork.h
+//  HHAFNetwork
+//
+//  Created by LeAustinHan on 14-8-11.
+//  Copyright (c) 2014年 han. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HHAFNetwork : NSObject
+
+@end
---
+++ HHAFNetwork/HHAFNetwork/HHAFNetwork.m
@@ -0,0 +1,13 @@
+//
+//  HHAFNetwork.m
+//  HHAFNetwork
+//
+//  Created by LeAustinHan on 14-8-11.
+//  Copyright (c) 2014年 han. All rights reserved.
+//
+
+#import "HHAFNetwork.h"
+
+@implementation HHAFNetwork
+
+@end
